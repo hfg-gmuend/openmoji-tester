6b39733b1027849bbe9c18bd0622ffda42e2cbe6
Benedikt Groß <bg@benedikt-gross.de>
2019-07-30 14:58:28 +0200

    Evo: post /test endpoint

--- server.js
+++ server.js
@@ -2,67 +2,98 @@ const path = require('path');
 const fs = require('fs-extra');
 const express = require('express');
 const { exec } = require('child_process');
+const { map } = require('lodash');
 const multer = require('multer');
 const uuidv1 = require('uuid/v1');
 
 const port = process.env.PORT || 3000;
+const pathPublic = path.resolve(__dirname, 'public');
+const pathTmp = '/tmp';
+
 const app = express();
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, req._jobDir),
   filename: (req, file, cb) => cb(null, file.originalname)
 });
-const upload = multer({ storage: storage });
+const upload = multer({storage: storage});
 
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(pathPublic));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-
-app.get('/', (request, response) => {
-  response.sendFile(path.resolve(__dirname, 'public/index.html'));
+app.get('/', (request, res) => {
+  res.sendFile(path.resolve(pathPublic, 'index.html'));
 });
 
-app.post('/jobs', beforeUpload, upload.array('svgFiles'), afterUpload);
+app.post('/test',
+  prepareTmpDir,
+  upload.array('svgFiles'),
+  checkUpload,
+  prepareOpenmojiJson,
+  runTest,
+  sendReport
+);
 
-function beforeUpload(req, res, next) {
-  req._jobId = uuidv1();
-  req._jobDir = path.resolve(__dirname, 'public/jobs/', req._jobId);
+function checkUpload(req, res, next) {
+  const files = req.files;
+  if (!files) {
+    const err = new Error('Please choose files');
+    err.httpStatusCode = 500;
+    return next(err);
+  }
+  next();
+}
+
+function prepareTmpDir(req, res, next) {
+  req._jobId = 'openmoji-' + uuidv1();
+  req._jobDir = path.resolve(pathTmp, req._jobId);
   fs.ensureDir(req._jobDir, (err) => {
     if (err) return next(err);
     next();
   });
 }
 
-function afterUpload(req, res, next) {
+function prepareOpenmojiJson(req, res, next) {
   const files = req.files;
-  if (!files) {
-    const err = new Error('Please choose files');
-    err.httpStatusCode = 400;
-    return next(err);
-  }
-  res.json({files: files});
+  const openmojis = map(files, f => {
+    return {
+      "emoji": "",
+      "hexcode": path.basename(f.filename, '.svg'),
+      "group": "",
+      "subgroups": "",
+      "skintone": "",
+      "skintone_combination": "",
+      "skintone_base_emoji": "",
+      "skintone_base_hexcode": ""
+    }
+  });
+  fs.writeJson(path.join(req._jobDir, 'openmoji.json'), openmojis, err => {
+    if (err) return next(err);
+    next();
+  })
 }
 
-app.get('/test', (request, response) => {
+function sendReport(req, res, next) {
+  // res.json({files: req.files});
+  res.sendFile(path.join(req._jobDir, 'report.html'));
+}
+
+function runTest(req, res, next) {
   const cmd = [
     'node_modules/.bin/mocha',
-    // '--reporter mochawesome',
-    '--reporter-options', 'reportDir=public/jobs/13234234,reportFilename=report',
+    '--reporter mochawesome',
+    // https://github.com/adamgruber/mochawesome-report-generator#options
+    '--reporter-options', `quiet=true,reportDir=${req._jobDir},reportFilename=report,json=false,inline=true,reportTitle=OpenMoji-Tester`,
     'openmoji/test/*.js',
-    '--openmoji-data-json', '$PWD/public/jobs/13234234/openmoji.json',
-    '--openmoji-src-folder', '$PWD/public/jobs/13234234',
+    '--openmoji-data-json', `${req._jobDir}/openmoji.json`,
+    '--openmoji-src-folder', `${req._jobDir}`,
   ].join(' ');
   console.log(cmd);
   exec(cmd, (err, stdout, stderr) => {
-    if (err) {
-      console.error(`stderr: ${stderr}`);
-      return response.status(500).send('Something broke! :(')
-    }
-    console.log(`stdout: ${stdout}`);
-    response.send(stdout);
+    next();
   });
-});
+}
 
 const listener = app.listen(port, function() {
   console.log(`Your app is listening on localhost:${listener.address().port}`);
